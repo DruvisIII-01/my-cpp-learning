5045c8f89ffb4ca0da745a7c66215a2820e98973
DruvisIII-01 <251250038@smail.nju.edu.cn>
2026-03-20 16:45:11 +0800

    15

--- contests/ABC449-C.cpp
+++ contests/ABC449-C.cpp
@@ -1,19 +1,49 @@
 #include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
+using namespace std ;
 
-int main() {
-	int n, l, r;
-	cin >> n >> l >> r;
-	r++;
-	string s;
-	cin >> s;
-	ll ans = 0;
-	vector<int> cnt(26);
-	for (int i = 0; i < n; i++) {
-		if (i >= l) cnt[s[i - l] - 'a']++;
-		if (i >= r) cnt[s[i - r] - 'a']--;
-		ans += cnt[s[i] - 'a'];
+#define int long long
+
+signed main() {
+	int n0, l, r ; cin >> n0 >> l >> r ;
+	string s ; cin >> s ;
+	vector <int> hsh[26] ;
+	for(int i = 0 ; i < n0 ; i++) {
+		hsh[s[i] - 'a'].push_back(i) ;
 	}
-	cout << ans << '\n';
+	int cnt = 0 ;
+	for(int t = 0 ; t < 26 ; t++) {
+		vector <int> a ; 
+		for(auto x : hsh[t]) a.push_back(x) ;
+		int n = a.size() ;
+		if(n <= 1) continue ;
+		for(int i = 0 ; i < n ; i++){
+			//枚举数组a中每一个每一个成员作为起点
+			//用二分查找找到第一个j
+			//s.t. a[j] - a[i] >= l ;
+			if(a[n - 1] - a[i] < l) break ;
+			int L1 = i, R1 = n ;
+			while(R1 != L1 + 1){
+				int M1 = (R1 + L1) >> 1 ;
+				if(a[M1] - a[i] >= l) R1 = M1 ;
+				else L1 = M1 ;
+			}
+			//a[R1] 是第一个满足 a[j] - a[i] >= l 的元素
+			//再用二分查找找到第一个j
+			//s.t. a[j] - a[i] > r ;
+			if(a[n - 1] - a[i] <= r) {
+				cnt += n - R1 ;
+				continue ;
+			}
+			int L2 = i, R2 = n ;
+			while(R2 != L2 + 1){
+				int M2 = (R2 + L2) >> 1 ;
+				if(a[M2] - a[i] > r) R2 = M2 ;
+				else L2 = M2 ;
+			}
+			//a[R2 - 1] 是最后一个满足a[j] - a[i] <= r 的元素
+			cnt += R2 - R1 ; 
+		}
+	}
+	cout << cnt << endl ;
+	return 0;
 }
