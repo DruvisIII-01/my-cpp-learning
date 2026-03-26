d981b37cada9958b2aaf619805e0e61c082b78e0
DruvisIII-01 <251250038@smail.nju.edu.cn>
2026-03-26 23:40:54 +0800

    3.26 1st

---
+++ contests/nowaday/CF790-H2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define int long long
+
+void solve(){
+    
+}
+
+signed main() {
+    int T ;cin >> T ;
+    while(T--) {
+        solve() ;
+        cout << endl ;
+    }
+    return 0;
+}
